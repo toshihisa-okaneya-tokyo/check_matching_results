fff61382f4c1cb688c0af5a4e538023ba634b0e1
Toshihisa Okaneya <t.okaneya@bloomingk2.com>
2025-02-12 20:11:20 +0900

    refactor: json parse処理を関数に分離

--- check_matching_results.sh
+++ check_matching_results.sh
@@ -35,14 +35,10 @@ get_prefix() {
 download_succeeded_json() {
     local target_date="$1"
     local work_dir="$2"
-    rm -rf "${work_dir}"
-    mkdir -p "${work_dir}"
-
     local prefix
     prefix=$(get_prefix "${target_date}")
     aws s3 sync --profile=dev2 "${prefix}result/" "${work_dir}" --exclude "*" --include "*SUCCEEDED_0.json"
 
-    cd "${work_dir}"
     find "${work_dir}" -name 'SUCCEEDED_0.json' | while read -r file; do
         # 親ディレクトリのパスとUUID部分を取得
         parent_dir=$(dirname "${file}")
@@ -69,6 +65,21 @@ download_succeeded_json() {
     done
 }
 
+parse_json() {
+    local work_dir="$1"
+
+    find "${work_dir}" -name SUCCEEDED_0.json -print0 | while IFS= read -r -d '' file; do
+        resource_type=$(basename "$(dirname "${file}")")
+
+        count=$(jq . "${file}" | grep 'status\\":500' | sed 's/^ *"Output": *"//' | wc -l)
+        # status: 500のログを抽出し、resource_type ごとのファイルに出力
+        output_file="${work_dir}/${resource_type}_${target_date}.log"
+        jq . "${file}" | grep 'status\\":500' | sed 's/^ *"Output": *"//' | sed 's/",$//' | sed 's#\\##g' | jq . >"${output_file}"
+
+        printf "%-10s - Number of status 500 data: %d\n" "${resource_type}" "${count}"
+    done
+}
+
 main() {
     local target_date
     target_date=$(get_target_date "${1:-}")
@@ -78,16 +89,12 @@ main() {
 
     local work_dir
     work_dir=$(get_work_dir "${target_date}")
+    rm -rf "${work_dir}"
+    mkdir -p "${work_dir}"
+    cd "${work_dir}"
     download_succeeded_json "${target_date}" "${work_dir}"
-    find "${work_dir}" -name SUCCEEDED_0.json -print0 | while IFS= read -r -d '' file; do
-        resource_type=$(basename "$(dirname "${file}")")
+    parse_json "${work_dir}"
 
-        count=$(jq . "${file}" | grep 'status\\":500' | sed 's/^ *"Output": *"//' | wc -l)
-        # jq と grep の結果を resource_type ごとのファイルに出力
-        jq . "${file}" | grep 'status\\":500' | sed 's/^ *"Output": *"//' | sed 's/",$//' | sed 's#\\##g' | jq . >"${work_dir}/${resource_type}_${target_date}.log"
-
-        printf "%-10s - Number of status 500 data: %d\n" "${resource_type}" "${count}"
-    done
     echo "End download and parse daily batch result successfully."
     echo "Detailed result is under ${work_dir}/"
 }
